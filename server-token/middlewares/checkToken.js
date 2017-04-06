2fe51d4e5a41a682c8b5a67d741cf8e99b69820c
Selvin11 <528370630@qq.com>
2017-04-06 20:13:41 +0800

    :bug: checktoken引起的请求500错误

--- server-token/middlewares/checkToken.js
+++ server-token/middlewares/checkToken.js
@@ -1,25 +1,26 @@
 // 监测 token 是否过期
 const jwt = require('jsonwebtoken')
 module.exports = function(req, res, next) {
-  let token = req.headers['authorization'].split(' ')[1]
+  if (req.headers['authorization']) {
+    let token = req.headers['authorization'].split(' ')[1]
 
-
-  
-  // 解构 token，生成一个对象 { name: xx, iat: xx, exp: xx }
-  
-  let decoded = jwt.decode(token)
-  console.log(decoded)
-  // 监测 token 是否过期
-  if (token && decoded.exp <= Date.now() / 1000) {
-    return res.json({
-      code: 401,
-      token: false,
-      error: 'token过期，请重新登录'
-    })
-  }else{
-    return res.json({
-      token: true
-    })
+    // 解构 token，生成一个对象 { name: xx, iat: xx, exp: xx }
+    
+    let decoded = jwt.decode(token)
+    console.log(decoded)
+    // 监测 token 是否过期
+    if (token && decoded.exp <= Date.now() / 1000) {
+      return res.json({
+        code: 401,
+        token: false,
+        error: 'token过期，请重新登录'
+      })
+    }else{
+      return res.json({
+        token: true
+      })
+    }
   }
+  
   next();
 }
